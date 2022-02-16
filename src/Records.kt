e027c7b0e8f002d3181152dd960f421c38f55ae6
sabdiogodana <sabdiyogodanaj@gmail.com>
2022-02-16 20:40:28 +0300

    first commit

---
+++ src/Records.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var name:String ="sabdio godana"
+     println(name)
+    var age:Byte = 21
+    println(age)
+    var phoneNumber= "0717785300"
+     println(phoneNumber)
+    var weight:String= "52kg"
+     println(weight)
+    var kenyan : Boolean = true
+    println(kenyan)
+    var notkenyan : Boolean = false
+     println(notkenyan)
+}
